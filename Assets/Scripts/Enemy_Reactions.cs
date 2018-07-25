88dfba07f999b1b9b65452ca2adad13edbda1df1
Akis-GitHub <toulkeridis@outlook.com>
2018-07-25 19:31:26 +0300

    small enemy-reactions script changes

--- Assets/Scripts/Enemy_Reactions.cs
+++ Assets/Scripts/Enemy_Reactions.cs
@@ -1,68 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Enemy_Reactions : MonoBehaviour {
-
-    public Transform _Player;
-    public float MaxDistToChange;
-    public float MinDistToChange;
-    SpriteRenderer e_SpriteRenderer;
+public class Enemy_Reactions : MonoBehaviour
+{
+    public Transform Player;
+    public float range;
+    //public float MaxDistToChange;
+    //public float MinDistToChange;
     public Color colorBlue;
     public Color colorGreen;
-    public bool IamBlue ;
-    public bool ChangerColor;
-    
-    // Use this for initialization
-    void Start ()
+    public bool IamBlue = false;
+    public bool ChangeColor = true;
+    SpriteRenderer e_SpriteRenderer;
+
+
+
+
+    void Start()
     {
         e_SpriteRenderer = GetComponent<SpriteRenderer>();
-        //set color at the start
-        if (IamBlue)
-        {
-            e_SpriteRenderer.color = colorBlue;
-            gameObject.tag = "BlueKiller";
-        }
-        else
-        {
-            e_SpriteRenderer.color = colorGreen;
-            gameObject.tag = "GreenKiller";
-        }
-    }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        //checks the distance between this and the player
-        var distance = gameObject.transform.position.x - _Player.transform.position.x;
 
-        if (distance<= MaxDistToChange && distance>= MinDistToChange&&ChangerColor)
-        {
-            ChangeColor();
-           
-        }
-        
-        //change color acordingly
-        //if (IamBlue)
-        //{
-        //    e_SpriteRenderer.color = colorBlue;
-        //}
-        //else
-        //{
-        //    e_SpriteRenderer.color = colorGreen;
-        //}
+        //set color at the start
+        SetColor();
     }
-    void ChangeColor()
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ChangeColor)
+            return; //(do not calculate distance for no reason)
+
+
+        //checks the distance between this and the player
+       // var distance = ;
+
+        if (Player.transform.position.x>=gameObject.transform.position.x - range)//>= distance <= MaxDistToChange && distance >= MinDistToChange)
+        {
+            ChangeColor = false; //do not change again
+            IamBlue = !IamBlue;
+            SetColor();
+        }
+    }
+
+    private void SetColor()
     {
         if (IamBlue)
-        {
-            e_SpriteRenderer.color = colorGreen;
-            gameObject.tag = "GreenKiller";
-        }
-        else
         {
             e_SpriteRenderer.color = colorBlue;
             gameObject.tag = "BlueKiller";
         }
+        else
+        {
+            e_SpriteRenderer.color = colorGreen;
+            gameObject.tag = "GreenKiller";
+        }
     }
+
+
+
 }
