57d6c593376a10860b858d4df5bf93312369c8c7
Akis-GitHub <toulkeridis@outlook.com>
2018-07-25 16:13:53 +0300

    I merely wrote a couple of comments
    We should add comments to our code to make it easier for others to understand

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,25 +2,24 @@
 
 public class Movement : MonoBehaviour
 {
+    public bool ShowBoxBounds = false; //this is for debug purposes only, should be false when publishing
     public Rigidbody2D Player;
     public float Speed;
     public float JumpForce;
     public Color colorBlue;
     public Color colorGreen;
-    SpriteRenderer m_SpriteRenderer;
     public bool IamBlue = true;
     public Transform CeilingCheck;
     public LayerMask GroundLayer;
-    public float OverlapRadius;
+
+    private SpriteRenderer m_SpriteRenderer;
     private bool IsTouchingGround;
     private bool IsTouchingCeiling;
-    public bool ShowBoxBounds = false; //this is for debug purposes only, should be false when publishing
     private Collider2D col;
     private Vector2 size;
 
 
 
-    //public float OverlapRadius;
     void Start()
     {
         Player = GetComponent<Rigidbody2D>();
@@ -49,14 +48,11 @@ public class Movement : MonoBehaviour
     {
         Vector2 pos = new Vector2(transform.position.x, transform.position.y);
         if (ShowBoxBounds)
-            DrawBoxForDebugPurposes(pos, size, transform.eulerAngles.z);
-        //IsTouchingGround = false;
-        Collider2D[] colliders = Physics2D.OverlapBoxAll(pos, size, transform.eulerAngles.z, GroundLayer);
+            DrawBoxForDebugPurposes(pos, size, transform.eulerAngles.z); //this draws a red rectangle to show the player's bounds
 
-        IsTouchingGround = colliders.Length != 0;
-        //foreach (Collider2D c in colliders)
-        //  if (c.tag == "Ground")
-        //    IsTouchingGround = true;
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(pos, size, transform.eulerAngles.z, GroundLayer); //this checks for any colliders within the boxy-area
+        IsTouchingGround = colliders.Length != 0; //at least one groundlayer collider present so we are in contact
+
 
 
 
@@ -75,9 +71,8 @@ public class Movement : MonoBehaviour
         //player moving alone
         Player.velocity = new Vector2(Speed, Player.velocity.y);
 
-
-
     }
+
 
     void Update()
     {
@@ -116,7 +111,7 @@ public class Movement : MonoBehaviour
         }
     }
 
-    //triggers
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         //death check
@@ -135,7 +130,7 @@ public class Movement : MonoBehaviour
             Player.velocity = new Vector2(Player.velocity.x, JumpForce * 2);
         }
     }
-    //COLLISSIONS ON COLLISION ENTER
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         //death check
@@ -149,17 +144,14 @@ public class Movement : MonoBehaviour
 
 
     void DrawBoxForDebugPurposes(Vector2 point, Vector2 size, float angle)
-    {
+    {//this draws a red rectangle to show the player's bounds
         var orientation = Quaternion.Euler(0, 0, angle);
-
         Vector2 right = orientation * Vector2.right * size.x / 2f;
         Vector2 up = orientation * Vector2.up * size.y / 2f;
-
         var topLeft = point + up - right;
         var topRight = point + up + right;
         var bottomRight = point - up + right;
         var bottomLeft = point - up - right;
-
         Color color = Color.red;
         Debug.DrawLine(topLeft, topRight, color);
         Debug.DrawLine(topRight, bottomRight, color);
