58f5ba59737daaa642758c0a0d8b4def623ef919
Akis-GitHub <toulkeridis@outlook.com>
2018-07-30 19:02:27 +0300

    Now the platform Y values are randomly set

--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -9,9 +9,9 @@ public class Room
     public GameObject[] MyEightPlatforms;
     public const float roomWidth = 50.25f; //because we are using 3 backgrounds each having a 1676 pixel width
     public const float platformWidth = 6.28125f; //(notice platformWidth * 8 = roomWidth)
+    public float lastPlatformY;
 
-    public Room(//GameObject myGameObject, 
-        GameObject platformPrefab, GameObject roomPrefab, float roomStartX)
+    public Room(GameObject platformPrefab, GameObject roomPrefab, float roomStartX, float yOfPreviousRoomsLastPlatform, bool isStartRoom=false)
     {
 
         GameObject roomGameObject = Object.Instantiate(roomPrefab);
@@ -22,22 +22,48 @@ public class Room
 
 
 
-
-
-
-
-
-
-        MyGameObject = roomGameObject;// myGameObject;
+        MyGameObject = roomGameObject;
 
 
 
         MyEightPlatforms = new GameObject[8];
 
+
+
         for (int i = 0; i < 8; i++)
         {
             MyEightPlatforms[i] = Object.Instantiate(platformPrefab);
-            MyEightPlatforms[i].transform.position = roomGameObject.transform.position - new Vector3(roomWidth / 2 - platformWidth / 2 - platformWidth * i, 0);
+
+
+
+            Vector3 determinsticPosition= roomGameObject.transform.position - new Vector3(roomWidth / 2 - platformWidth / 2 - platformWidth * i, 0);
+            MyEightPlatforms[i].transform.position = determinsticPosition;
+
+            if (isStartRoom && i==0)
+            {//we want the very first platform of our game to be at a fixed position
+            }
+            else
+            {//for every other platform we want a y that is the y of the previous platform plus/minus a small random value
+
+
+                float randomDifference = Random.Range(-1f,1f);
+
+                float newY = yOfPreviousRoomsLastPlatform + randomDifference;
+
+                if(newY>3.4f || newY<-3.4f)
+                {//out of allowed game bounds, go the other way
+
+                    newY -= 2 * randomDifference;
+                }
+
+                MyEightPlatforms[i].transform.position = new Vector3(determinsticPosition.x, newY);
+                    
+                    //new Vector3(roomWidth / 2 - platformWidth / 2 - platformWidth * i, newY);
+
+                if (i == 7)
+                    lastPlatformY = newY;
+            }
+            
         }
     }
 
@@ -62,7 +88,7 @@ public class RoomGenerator : MonoBehaviour
 
     void Start()
     {
-        _existingRooms = new List<Room> { new Room(platformPrefab, roomPrefab, -25.14f) }; //-25.14 because -16.76 + -16.76/2
+        _existingRooms = new List<Room> { new Room(platformPrefab, roomPrefab, -25.14f, -1, true) }; //-25.14 because -16.76 + -16.76/2  ,  the -1 is ignored here
 
         float screenHeight = 2.0f * Camera.main.orthographicSize;
         _screenWidth = screenHeight * Camera.main.aspect;
@@ -89,7 +115,7 @@ public class RoomGenerator : MonoBehaviour
         float removeRoomThresholdX = playerX - _screenWidth;
         float addRoomThresholdX = playerX + _screenWidth;
         float farthestRoomEndX = 0;
-
+        float yOfPreviousRoomsLastPlatform = -1;
         bool addRoom = true;
         foreach (var room in _existingRooms)
         {
@@ -103,6 +129,8 @@ public class RoomGenerator : MonoBehaviour
                 roomsToRemove.Add(room);
 
             farthestRoomEndX = Mathf.Max(farthestRoomEndX, roomEndX);
+
+            yOfPreviousRoomsLastPlatform = room.lastPlatformY;
         }
 
         foreach (var room in roomsToRemove)
@@ -115,7 +143,7 @@ public class RoomGenerator : MonoBehaviour
 
         if (addRoom)
         {
-            Room room = new Room(platformPrefab, roomPrefab, farthestRoomEndX);
+            Room room = new Room(platformPrefab, roomPrefab, farthestRoomEndX, yOfPreviousRoomsLastPlatform);
 
             _existingRooms.Add(room);
         }
