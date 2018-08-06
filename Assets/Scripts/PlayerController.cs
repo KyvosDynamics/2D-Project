810fc7b033758c4edd5c9b7e0b84e315cb1c351e
Akis-GitHub <toulkeridis@outlook.com>
2018-08-06 19:02:35 +0300

    It no longer relies on CollisionEnter/CollisionStay for autojumping but on horizontal raycasts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,150 +1,156 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private bool VisualizeRaycasting = true; //this is for debug purposes only, should be false when releasing
-    public float Speed;
-    public float JumpForce;
-    public LayerMask GroundLayer;
-
-    //by convention private fields start with the underscore '_' character followed by a lower-case letter
     [HideInInspector]
     public bool IsBlue = true;
-    private Rigidbody2D _player;
+    public float Speed;
+    public float JumpSpeed;
+    public LayerMask GroundLayer;
+    //by convention private fields start with the underscore '_' character followed by a lower-case letter
+    private Rigidbody2D _rigidbody;
     private SpriteRenderer _spriteRenderer;
     private bool _isTouchingGround;
-    private bool _isTouchingCeiling = false;
-    private Collider2D _collider;
-    private Vector2 _size;
+    //  private bool _isTouchingCeiling = false;
+    bool _isTouchingWall;
+    //  bool _isTouchingWall;
+    bool goUp = false;
+    private float _halfHeight;
+    private float _halfWidth;
 
 
 
     void Start()
     {
-        _player = GetComponent<Rigidbody2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
 
-        _collider = GetComponent<Collider2D>();
-        _size = new Vector2(_collider.bounds.size.x, _collider.bounds.size.y);
-
+        Collider2D _collider = GetComponent<Collider2D>();
+        //_halfHeight = / 2;
+        _halfWidth = _collider.bounds.size.x / 2;
 
         //set color at the start
         if (IsBlue)
             _spriteRenderer.color = Color.blue;// colorBlue;        
         else
-            _spriteRenderer.color = Color.green;// colorGreen;        
+            _spriteRenderer.color = Color.green;// colorGreen;      
+
+
+
+        //to see if the player has hit a wall let's create 4 horizontal raycasts (if we change either the player's or the platform's size 4 may be too few or too many)
+         numOfHorRaycasts = 4;
+        float fullHeight = _collider.bounds.size.y;// 2 * _halfHeight;
+
+         yIncr = fullHeight / (numOfHorRaycasts - 1);//minus one because for example 4 points form 3 intervals
+                                                     //lets do them from the bottom to the top
+
+        _halfHeight = fullHeight / 2;
     }
 
 
+    int numOfHorRaycasts;
+    float yIncr;
 
- 
+
 
     void FixedUpdate()
     {
+        Vector3 bottommostPoint = transform.position + Vector3.down * (_halfHeight +0.01f); //don't mind the 0.01f
+        Vector3 bottommostRightmostPoint = bottommostPoint + Vector3.right * _halfWidth;
 
 
-        /*
-        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+        float raycastingDistance = 0.1f;
 
-        //let's find the four vertices
-        var orientation = Quaternion.Euler(0, 0, transform.eulerAngles.z);
-        Vector2 right = orientation * Vector2.right * _size.x / 2f; //the right vector is the result of rotating half the square's horizontal axis by the square's z angle
-        Vector2 up = orientation * Vector2.up * _size.y / 2f; //the up vector is the result of rotating half the square's vertical axis by the square's z angle
-
-
-        var topLeft = pos + up - right;
-        var topRight = pos + up + right;
-        var bottomRight = pos - up + right;
-        var bottomLeft = pos - up - right;
-        //the above are ofcourse NOT absolute positions. They depend on the square's rotation (so for example if the player rotates 90 degrees clockwise the topleft vertex is actually top-right!
-
-
-        List<Vector2> verticesToRaycast = new List<Vector2>();
-        verticesToRaycast.Add(topLeft);
-        verticesToRaycast.Add(topRight);
-        verticesToRaycast.Add(bottomRight);
-        verticesToRaycast.Add(bottomLeft);
-
-
-        RemoveVertexWithSmallerY(verticesToRaycast);
-
-        //we want when the z angle is a multiple of 90 to raycast the top two vertices
-        //otherwise to raycast the top three vertices
-        float zAngle = transform.eulerAngles.z;
-        if (Mathf.Round(zAngle) % 90 == 0)
-        {//it is a multiple of 90, remove one more vertex
-
-            RemoveVertexWithSmallerY(verticesToRaycast);
-        }
-        */
-        //print(string.Format("Raycasting {0} vertices", verticesToRaycast.Count));
+        _isTouchingGround = Physics2D.Raycast(bottommostPoint, Vector2.down, raycastingDistance, GroundLayer);
+        Debug.DrawLine(bottommostPoint, bottommostPoint + Vector3.down * raycastingDistance, _isTouchingGround ? Color.red : Color.green);
 
 
 
 
-        float verticalRaycastingDistance = 0.1f + _size.y / 2;// 10;// _size.x / 10;
-        float horizontalRaycastingDistance = 0.1f + _size.x / 2;// 10;// _size.x / 10;
 
+       
 
-        _isTouchingCeiling = Physics2D.Raycast(transform.position, Vector2.up, verticalRaycastingDistance, GroundLayer);
-        Debug.DrawLine(transform.position, transform.position + Vector3.up * verticalRaycastingDistance, _isTouchingCeiling ? Color.red : Color.green);
-
-        /*
-        foreach (Vector2 v in verticesToRaycast)
+        _isTouchingWall = false;
+        for (int raycastIndex = 0; raycastIndex < numOfHorRaycasts && _isTouchingWall == false; raycastIndex++)
         {
-            bool vTouching = Physics2D.Raycast(v, Vector2.up, raycastingDistance, GroundLayer);
-
-            if (VisualizeRaycasting)
-                Debug.DrawLine(v, v + Vector2.up * raycastingDistance, vTouching ? Color.red : Color.green);
-
-            _isTouchingCeiling = _isTouchingCeiling | vTouching;
+            Vector3 point = bottommostRightmostPoint + Vector3.up * raycastIndex * yIncr;
+            _isTouchingWall = Physics2D.Raycast(point, Vector2.right, raycastingDistance, GroundLayer);
+            Debug.DrawLine(point, point + Vector3.right * raycastingDistance, _isTouchingWall ? Color.red : Color.green);// Color.white);
         }
-        */
 
 
 
 
 
-        //   Collider2D[] colliders = Physics2D.OverlapBoxAll(pos, _size, transform.eulerAngles.z, GroundLayer); //this checks for any colliders within the boxy-area
-        //  _isTouchingGround = colliders.Length != 0; //at least one groundlayer collider present so we are in contact
+
+        //   bool 
+        //  _isTouchingWall =
+
+
+
+        //        if (_isTouchingWallMiddle == false)
+        //      {
+        //        _isTouchingWallLow = Physics2D.Raycast(bottommostRightmostPoint, Vector2.right, raycastingDistance, GroundLayer);
+        //      Debug.DrawLine(bottommostRightmostPoint, bottommostRightmostPoint + Vector3.right * raycastingDistance, _isTouchingWallLow ? Color.red : Color.green);
+        //}
 
 
 
 
-        _isTouchingGround = Physics2D.Raycast(transform.position, Vector2.down, verticalRaycastingDistance, GroundLayer);
-        // if (VisualizeRaycasting)
-        Debug.DrawLine(transform.position, transform.position + Vector3.down * verticalRaycastingDistance, _isTouchingGround ? Color.red : Color.green);
-
-        //  _isTouchingGround = _isTouchingGround | vTouching;
 
 
 
-        _isTouchingWall = Physics2D.Raycast(transform.position + Vector3.down * _size.y / 2, Vector2.right, horizontalRaycastingDistance, GroundLayer);
-        Debug.DrawLine(transform.position + Vector3.down * _size.y / 2, transform.position + Vector3.down * _size.y / 2 + Vector3.right * horizontalRaycastingDistance, _isTouchingWall ? Color.red : Color.green);
-
-        float yVel = _player.velocity.y;
-
-        if (_isTouchingWall || goUp)
+        float yVel = _rigidbody.velocity.y;
+        if (jumpFromGround)
         {
-            goUp = false;
-            yVel = JumpForce/1.5f;
+            jumpFromGround = false;
+            yVel = JumpSpeed;
         }
-        //else
+        else if(jumpFromWall)
+        {
+            jumpFromWall = false;
+            yVel = JumpSpeed - AutoClimbSpeed;
+
+        }
+        else if(_isTouchingWall)
+        {//auto climb
+
+            yVel = AutoClimbSpeed;
+
+        }
 
 
-        //player moving alone
-        _player.velocity = new Vector2(Speed, yVel);
+
+        _rigidbody.velocity = new Vector2(Speed, yVel);
+
+
+
+
+
+        //   if (_isTouchingWallLow || goUp)
+        // {
+        //   goUp = false;
+        // yVel = JumpForce / 1.5f;
+        // }
+
+
+
+
+
 
     }
-    bool _isTouchingWall;
+    public float AutoClimbSpeed = 2f;
 
+
+
+
+
+    bool switchcolor = false;
 
 
 
     void Update()
     {
-        _player.velocity = new Vector2(Speed, _player.velocity.y);
 
 
 
@@ -180,13 +186,18 @@ public class PlayerController : MonoBehaviour
 
 
         //jump check
-        if (Input.GetKey(KeyCode.Space) && _isTouchingGround && !_isTouchingCeiling)
+        if (Input.GetKey(KeyCode.Space))// && _isTouchingWall==false)// && !_isTouchingCeiling)
         {
-            _player.velocity = new Vector2(_player.velocity.x, JumpForce);
+            if (_isTouchingGround)
+                jumpFromGround = true;
+            else if (_isTouchingWall)
+                jumpFromWall = true;
             //_isTouchingGround = false;
         }
     }
 
+    bool jumpFromGround = false;
+    bool jumpFromWall = false;
 
 
 
@@ -214,7 +225,7 @@ public class PlayerController : MonoBehaviour
                 break;
 
             case "Ponger":
-                _player.velocity = new Vector2(_player.velocity.x, JumpForce * 1.5f);
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, JumpSpeed * 1.5f);
                 break;
 
             case "Killer":
@@ -223,25 +234,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.GetContact(0).otherCollider.transform.gameObject.name == "RightSideCollider")
-        {
-            goUp = true;// _player.velocity = new Vector2(Speed, _player.velocity.y + 0.1f);
-            print("yeah");
-        }
 
-}
-    bool goUp = false;
 
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.GetContact(0).otherCollider.transform.gameObject.name == "RightSideCollider")
-        {
-            goUp = true;// _player.velocity = new Vector2(Speed, _player.velocity.y + 0.1f);
-            print("yeah");
-        }
-
-    }
 
 }
