8867ce248c035bc4856f9d259ab60c5da682f6c0
Akis-GitHub <adaplect@outlook.com>
2018-08-23 17:59:27 +0300

    Now the menu button does a silly little effect

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -3,12 +3,38 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public GameObject spinningImage;
 
 
+
+    bool spinImage = false;
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        spinImage = true;
+
+
+    }
+
+    int rotation = 360;
+
+
+
+    void FixedUpdate()
+    {
+        if (spinImage == false)
+            return;
+
+
+        rotation -= 10;
+        spinningImage.transform.rotation = Quaternion.Euler(Vector3.forward * rotation);
+
+        if (rotation == 0)
+
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+
     }
 
 
