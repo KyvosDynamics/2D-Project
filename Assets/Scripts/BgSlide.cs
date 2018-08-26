a15b0afa8df6b5238c8845e817d7e66f148e1cc6
Akis-GitHub <adaplect@outlook.com>
2018-08-26 15:11:40 +0300

    Very small code changes

---
+++ Assets/Scripts/BgSlide.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BgSlide : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        StartCoroutine("Scroll");
+    }
+
+    IEnumerator Scroll()
+    {
+        const float scrollSpeed = 0.005f;
+
+        RawImage rawImage = GetComponent<RawImage>();
+        Rect uvRect = rawImage.uvRect;
+
+        int time = 0;
+
+        while (true)
+        {
+            float x = Mathf.Repeat(time * scrollSpeed, 1);
+            time++;
+            uvRect.x = x;
+            rawImage.uvRect = uvRect;
+            yield return new WaitForSeconds(0.025f);
+        }
+    }
+
+
+
+}
