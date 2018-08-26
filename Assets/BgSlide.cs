fca3000b5f72caf798de6ef2cc6d376c636bcc0d
Akis-GitHub <adaplect@outlook.com>
2018-08-26 12:41:10 +0300

    Now the menu looks a bit nicer

---
+++ Assets/BgSlide.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BgSlide : MonoBehaviour
+{
+
+    private float scrollSpeed = 1;
+    private Rect uvrect;
+    private float time = 0;
+
+
+    void Start()
+    {
+        uvrect = GetComponent<RawImage>().uvRect;
+        StartCoroutine("Scroll");
+    }
+
+    IEnumerator Scroll()
+    {
+        while (true)
+        {
+            float x = Mathf.Repeat(time * 0.1f * scrollSpeed, 1);
+            time += 0.02f;
+            uvrect.x = x;
+            GetComponent<RawImage>().uvRect = uvrect;
+            yield return new WaitForSeconds(0.025f);
+        }
+    }
+
+
+
+}
