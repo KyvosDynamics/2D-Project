3bd6d6c9da25660321febe22cabd1a4264afc6fd
Akis-GitHub <adaplect@outlook.com>
2018-09-02 19:00:15 +0300

    Now the front-effect and the front of the Portal prefab are on a new layer that is in front of the player

---
+++ Assets/PortalController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PortalController : MonoBehaviour {
+
+
+public    SpriteRenderer _frontEffectRenderer;
+    public SpriteRenderer _backEffectRenderer;
+
+	// Use this for initialization
+	void Start () {
+
+        _frontEffectRenderer.color = _backEffectRenderer.color = Color.green;
+
+    }
+	
+
+
+
+}
